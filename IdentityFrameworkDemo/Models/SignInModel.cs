0b806fe9e2e21091cdf24231dff730dd25cb0089
Narender <tanwarnarender086@gmail.com>
2021-04-26 23:56:39 +0530

    Account Controller with View is added

--- IdentityFrameworkDemo/Models/SignInModel.cs
+++ IdentityFrameworkDemo/Models/SignInModel.cs
@@ -10,11 +10,11 @@ namespace IdentityFrameworkDemo.Models
     {
         [Required(ErrorMessage ="Plz Enter Email"),EmailAddress]
         public String Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Plz Enter Email")]
         [DataType(DataType.Password)]
         public String Password { get; set; }
 
-        [Required]
+        
         [Display(Name ="Remember Me")]
         public bool RememberMe { get; set; }
     }
