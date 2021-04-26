4453728d3a83a3b18c96b1c48afa6a4931cc572e
Narender <tanwarnarender086@gmail.com>
2021-04-26 22:05:12 +0530

    First Commit

---
+++ IdentityFrameworkDemo/Models/SignInModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityFrameworkDemo.Models
+{
+    public class SignInModel
+    {
+        [Required(ErrorMessage ="Plz Enter Email"),EmailAddress]
+        public String Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Required]
+        [Display(Name ="Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
