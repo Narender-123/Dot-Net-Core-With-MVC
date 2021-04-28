9980c926161813f461e2184281c3c748d7f54ee0
Narender <tanwarnarender086@gmail.com>
2021-04-28 18:21:42 +0530

    Signup Psost Method Is Added

--- IdentityFrameworkDemo/Controllers/AccountController.cs
+++ IdentityFrameworkDemo/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityFrameworkDemo.Models;
 
 namespace IdentityFrameworkDemo.Controllers
 {
@@ -12,6 +13,20 @@ namespace IdentityFrameworkDemo.Controllers
         public IActionResult SignUp()
         {
             return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult SignUp(SignUpModel signUpModel) 
+        {
+            //If ModelState is Valid That means to Store in the Database
+            if (ModelState.IsValid)
+            {
+                //Write Your Code
+                ModelState.Clear();
+                return View();
+            }
+            return View(signUpModel);
         }
 
         [Route("login")]
