8347a2000d3c287e2a33516505985a734754fee9
Narender <tanwarnarender086@gmail.com>
2021-05-21 14:46:51 +0530

    FindSmallest.cs is added Have all the basic aggregate Methods in it

---
+++ ConsoleApp1/FindSmallest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class FindSmallest
+    {
+        static void Main() 
+        {
+            int[] Numbers = { 21, 22, 23, 24, 25, 26, 27, 28, 29, 1, 2, 3, 4, 5, 6, 7, 8, 45, 9, 10, 11, 12, 13, 14 };
+            
+            //Without Using Linq Find the Smallest Number--------------------------------------------------------------
+            int? smallest = null;
+            foreach (int n in Numbers)
+            {
+                if (!smallest.HasValue || smallest>n) 
+                {
+                    smallest = n;
+                }
+            }
+            Console.WriteLine("The Smallest Number = "+smallest);
+
+            //Without Using Linq Find the Smallest Even Number----------------------------------------------------------
+            int? smallestEven = null;
+            foreach (int n in Numbers)
+            {
+                //First Check For Even
+                if (n%2==0) 
+                { 
+                    if (!smallestEven.HasValue || smallestEven > n)
+                    {
+                        smallestEven = n;
+                    }
+                }
+            }
+            Console.WriteLine("\nThe SmallestEven Number = " + smallestEven);
+
+            //++++++++++++++++++++++++++++++++++LINQ  Operators (Aggrigate Operators) ++++++++++++++++++++++++++++++++++
+            //With Using of Linq Find the Smallest Number---------------------------------------------------------------
+            int smallest1 = Numbers.Min();
+            Console.Write("\nThe Smallest Number is = "+smallest1);
+
+            //With Using of Linq Find the Greatest Number---------------------------------------------------------------
+            int Greatest1 = Numbers.Max();
+            Console.Write("\nThe Greatest Number is = " + Greatest1);
+
+            //With Using of Linq Find the GreatestEven Number------------------------------------------------------------
+            int GreatestEven1 = Numbers.Where(x => x%2==0).Max();
+            Console.Write("\nThe Greatest Even Number is = " + GreatestEven1);
+
+            //With Using of Linq Find the Sum of the Array/Collection/Set/Total------------------------------------------
+            int Sum1 = Numbers.Sum();
+            Console.Write("\nThe Total Sum of the Numbers  = " + Sum1);
+
+            //With Using of Linq Find the Number of Items/Count---------------------------------------------------------
+            int count1 = Numbers.Count();
+            Console.Write("\nThe Total Count of the Numbers  = " + count1);
+
+
+            //With Using Linq Find the Smallest Even Number--------------------------------------------------------------
+            int smallestEven1 = Numbers.Where(x => x % 2 == 0).Min();
+            Console.WriteLine("\nThe SmallestEven Number = " + smallestEven1);
+
+            //With Using Linq Find the Average of Even Number-------------------------------------------------------------
+            double avgEven1 = Numbers.Where(x => x % 2 == 0).Average();
+            Console.WriteLine("\nThe Average of Even Number = " + avgEven1);
+
+            //============================================================================================================
+
+            //Find the Shorest Country Characters name Without Using Linq-------------------------------------------------
+            string[] Countries = {"Britin","USA","RSA","UK","India" };
+
+            int? shortestCountry = null;
+            foreach (string country in Countries) 
+            {
+                if (!shortestCountry.HasValue || country.Length<shortestCountry)
+                {
+                    shortestCountry = country.Length;
+                }
+            }
+            Console.WriteLine("Shortest Counrty name has {0} Characters in its Name",shortestCountry);
+
+            //Find the Shorest Country Characters name With Using Linq---------------------------------------------------------
+            Console.WriteLine("Shortest Counrty name has {0} Characters in its Name", Countries.Min(country => country.Length));
+
+            //Find the Shorest Country Characters name With Using Linq---------------------------------------------------------
+            Console.WriteLine("Greatest Counrty name has {0} Characters in its Name", Countries.Max(country => country.Length));
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void main()
         {
             String name = "Narender";
 
